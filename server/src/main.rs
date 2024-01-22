2876c0904a00a857dfff192b3f7b18022b28ae2e
Oscar Cederberg <os7138ce-s@student.lu.se>
2024-01-22 14:10:17 +0100

    Finish prototype of server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,12 @@
-use std::collections::HashMap;
-use std::io;
-use std::net::{SocketAddr, UdpSocket};
 use rand::Rng;
+use std::collections::HashMap;
+use std::net::{SocketAddr, UdpSocket};
 
 const PEER_HOST: [u8; 4] = *b"HOST";
 const PEER_JOIN: [u8; 4] = *b"JOIN";
 const MAX_MSG_LEN: usize = 1024;
 const CODE_LEN: usize = 5;
+
 
 struct HolePunchServer {
     socket: UdpSocket,
@@ -14,16 +14,80 @@ struct HolePunchServer {
 }
 
 impl HolePunchServer {
-    fn run(self) -> Result<(), io::Error> {
-        let HolePunchServer { socket, mut hosts } = self;
+    fn send_response(&self, response: &[u8], address: &SocketAddr) -> bool {
+        if let Err(error) = self.socket.send_to(response, address) {
+            println!("Failed to send bytes: {}", error);
+            false
+        } else {
+            println!("Sent {} bytes to {}: {:?}", response.len(), address, response);
+            true
+        }
+    }
 
+    fn add_host(&mut self, address: &SocketAddr) {
+        let mut code: [u8; CODE_LEN] = [0; CODE_LEN];
+        let mut rng = rand::thread_rng();
+
+        loop {
+            code.iter_mut().for_each(|v| {
+                *v = loop {
+                    let character = rng.gen_range(b'A'..=b'Z');
+                    if !matches!(character, b'A' | b'E' | b'I' | b'O' | b'U' | b'Y') {
+                        break character;
+                    }
+                }
+            });
+
+            if !self.hosts.contains_key(&code) {
+                break;
+            }
+        }
+
+        let mut response = code.to_vec();
+        response.push(b'\n');
+        if self.send_response(&response, address) {
+            self.hosts.insert(code, *address);
+        }
+    }
+
+    fn join_host(&mut self, msg: &[u8], size: usize, address: &SocketAddr) {
+        if size < PEER_JOIN.len() + CODE_LEN + 1 {
+            println!("Join request too short: {:?}", msg);
+            self.send_response(b"Join request too short\n", address);
+            return;
+        }
+
+        let code = &msg[(PEER_JOIN.len() + 1)..=(PEER_JOIN.len() + CODE_LEN)];
+
+        if !code.iter().all(|v| {
+            v.is_ascii_uppercase() && !matches!(v, b'A' | b'E' | b'I' | b'O' | b'U' | b'Y')
+        }) {
+            println!("Illegal join code: {:?}", code);
+            self.send_response(b"Illegal join code\n", address);
+            return;
+        }
+
+        if !self.hosts.contains_key(code) {
+            println!("No host available with join code: {:?}", code);
+            self.send_response(b"No host available with join code\n", address);
+            return;
+        }
+
+        let host = self.hosts.remove(code).unwrap();
+
+        if self.send_response(format!("Client: {:?}", address).as_bytes(), &host) {
+            self.send_response(format!("Host: {:?}", host).as_bytes(), address);
+        }
+    }
+
+    fn run(mut self) {
         loop {
             // Receive messages from clients
             let mut buf = vec![0; MAX_MSG_LEN];
 
             let size;
             let peer_address;
-            if let Ok((recv_size, recv_address)) = socket.recv_from(&mut buf) {
+            if let Ok((recv_size, recv_address)) = self.socket.recv_from(&mut buf) {
                 size = recv_size;
                 peer_address = recv_address;
             } else {
@@ -37,36 +101,10 @@ impl HolePunchServer {
 
             if msg.starts_with(&PEER_HOST) {
                 println!("HOST request received");
-
-                let mut code:[u8; CODE_LEN] = [0; CODE_LEN];
-                let mut rng = rand::thread_rng();
-
-                loop {
-                    code.iter_mut().for_each(|v| {
-                        *v = loop {
-                            let character = rng.gen_range(b'A'..=b'Z');
-                            if !matches!(character, b'A' | b'E'| b'I'| b'O'| b'U'| b'Y') {
-                                break character;
-                            }
-                        }
-                    });
-
-                    if !hosts.contains_key(&code) {
-                        break;
-                    }
-                }
-
-                let mut response = code.to_vec();
-                response.push(b'\n');
-
-                if let Err(error) = socket.send_to(&response, peer_address) {
-                    println!("Failed to receive bytes: {}", error);
-                } else {
-                    println!("Sent {} bytes to {}: {:?}",size, peer_address, response);
-                    hosts.insert(code, peer_address);
-                }
+                self.add_host(&peer_address);
             } else if msg.starts_with(&PEER_JOIN) {
-                todo!();
+                println!("JOIN request received");
+                self.join_host(msg, size, &peer_address);
             }
         }
     }
@@ -81,5 +119,5 @@ fn main() {
     let hosts = HashMap::new();
     let server = HolePunchServer { socket, hosts };
 
-    let _ = server.run();
+    server.run();
 }
